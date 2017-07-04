21cb9eb9eaa63cdca6e846fa3fc51534879f8d88
Léo POULIN <leo@goodbarber.net>
2017-07-04 11:53:52 +0200

    test script

---
+++ test/test_populate.py
@@ -0,0 +1,19 @@
+
+import world
+import entities
+
+def dump_board(board):
+    print('\n'.join(''.join(str(x) for x in y) for y in board))
+
+def main_test():
+    game = world.World()
+    game.spawn([
+        (entities.LINE_OF_20, 0, 10)
+        ])
+    for _ in range(10):
+        game.populate()
+        dump_board(game.map)
+    return 0
+
+if __name__ == '__main__':
+    main_test()
