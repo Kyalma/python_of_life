2ee8d07a96586a6d573811246fb01b18882aba5d
Léo POULIN <leo@goodbarber.net>
2017-07-04 13:20:07 +0200

    added args to test

--- test/test_populate.py
+++ test/test_populate.py
@@ -4,8 +4,10 @@ Test script for Travis CI
 
 import sys
 import os
+import argparse
 
 sys.path.append(os.path.abspath('./'))
+
 
 from gameoflife import world
 import entities
@@ -14,15 +16,25 @@ import entities
 def dump_board(board):
     print('\n'.join(''.join(str(x) for x in y) for y in board))
 
-def main_test():
+def main_test(args):
     game = world.World()
     game.spawn([
         (entities.LINE_OF_20, 0, 10)
         ])
-    for _ in range(10):
+    dump_board(game.map)
+    for _ in range(args.gen):
         game.populate()
         dump_board(game.map)
     return 0
 
 if __name__ == '__main__':
-    main_test()
+    parser = argparse.ArgumentParser(
+        prog="test_populate.py",
+        description="Generate populations")
+    parser.add_argument(
+        "-gen",
+        type=int,
+        default=1,
+        action="store")
+    args = parser.parse_args()
+    main_test(args)
